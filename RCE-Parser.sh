d1f02267d946aaf1e66326445dc44c9a905693ea
fancysauced <37448391+fancysauced@users.noreply.github.com>
2021-02-16 12:45:15 -0500

    Update RCE-Parser.sh

--- RCE-Parser.sh
+++ RCE-Parser.sh
@@ -3,11 +3,15 @@
 # Parse NMAP files for easy RCE Ports.
 # Work in progress
 
-input"$1"
-output"$2"
+input="$1"
+output="$2"
 
-function ascii(){
+function show_usage(){
+    echo 'Usage: ./RCE-Parser.sh inputfile.nmap outputfile\n'
+}
+
 # Lame attempt at ASCII art to make it Super1337.
+function ascii_art(){
     echo '+--------------------------------+'
     echo '|======[ RCE-Cream-Parcer ]======|'
     echo '+--------------------------------+'
@@ -35,9 +39,13 @@ function ascii(){
     echo 'Usage: ./RCE-Parser.sh inputfile.nmap outputfile\n'
     echo ''
     echo 'If you want to nmap specifically for these ports:'
-    echo 'nmap -Pn -sS -sV --open -p 1090,1098,1099,4444,11099,47001,47002,10999,12721,7000-7004,8000-8003,9000-9003,9503,7070,7071,45000,45001,8686,9012,50500,4848,11111,4444,4445,4786,5555,5556,8880,8088,6379,2375,8983,8383,4990,9000,8500,6066,5000,8080,3300,6129,6970 -iL targets.txt -oA rce-ports'
+    echo 'nmap -Pn -sS -sV --open -p 1090,1098,1099,4444,11099,47001,47002,10999,12721,7000-7004,800>
 }
 
 function rce_parser (){
-    cat $input| grep -i 'Nmap scan report for \|PORT\|1090/tcp\|1098/tcp\|1099/tcp\|4444/tcp\|11099/tcp\|47001/tcp\|47002/tcp\|10999/tcp\|12721/tcp\|7000/tcp\|7001/tcp\|7002/tcp\|7003/tcp\|7004/tcp\|8000/tcp\|8001/tcp\|8002/tcp\|8003/tcp\|9000/tcp\|9001/tcp\|9002/tcp\|9003/tcp\|9503/tcp\|7070/tcp\|7071/tcp\|45000/tcp\|45001/tcp\|8686/tcp\|9012/tcp\|50500/tcp\|4848/tcp\|11111/tcp\|4444/tcp\|4445/tcp\|4786/tcp\|5555/tcp\|5556/tcp\|8880/tcp\|8088/tcp\|6379/tcp\|2375/tcp\|8983/tcp\|8383/tcp\|4990/tcp\|8500/tcp\|6066/tcp\|5000/tcp\|8080/tcp\|3300/tcp\|6129/tcp\|6970/tcp\|weblogic\|jdwp\|jmx\|glassfish\|jboss\|redis\|sap\|dameware\|coldfusion\|jenkins\|docker\|flink\|spark\|hadoop\|manageengine\|hashicorp\|websphere\|solr\|portainer\|java-rmi\|rmi-registry\|hp data protector\|cisco smart install\|cisco unified communications manager\|Service Info:'| sed 's/Nmap scan report for /\n/g' > $output
+    cat $input| grep -i 'Nmap scan report for \|PORT\|1090/tcp\|1098/tcp\|1099/tcp\|4444/tcp\|11099/>
 }
+
+if [ "$#" -ne 2 ]; then
+    show_usage
+fi
